c709a822b7ab3d536818ddcddef1421093f0945c
Adam Magnusson <admag2001@gmail.com>
2023-03-01 22:42:16 +0100

    started on ALU

---
+++ hardware/Plane/ALU/ALU.vhd
@@ -0,0 +1,21 @@
+entity ALU is
+  generic (
+    OP_Length : integer := 2;
+  );
+
+  port (
+    clock : std_logic;
+    OP : std_logic_vector(OP_Length-1 downto 0)
+    
+  );
+end ALU;
+
+architecture arch of ALU is
+
+
+
+begin
+
+
+
+end architecture ; -- arch
