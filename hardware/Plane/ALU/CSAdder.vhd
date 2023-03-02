598def0ce0af4ff79b20e2863bd1f43483f27970
Adam Magnusson <admag2001@gmail.com>
2023-03-02 16:51:37 +0100

    added library, ports and generics to ALU files

--- hardware/Plane/ALU/CSAdder.vhd
+++ hardware/Plane/ALU/CSAdder.vhd
@@ -1,7 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity CSAdder is
+  generic(
+    input_len : integer := 64;
+    VMUL_len : integer := 3
+  );
   port (
+    a, b, c : std_logic_vector (input_len-1 downto 0);
+    VMUL : std_logic_vector (VMUL_len-1 downto 0);
     
-  ) ;
+  );
 end CSAdder;
 
 architecture arch of CSAdder is
