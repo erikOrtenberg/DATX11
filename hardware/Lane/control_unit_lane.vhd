5be37888164e5cc34def68034249a571e021fcba
Erik Örtenberg <erik.ortenberg@gmail.com>
2023-03-17 18:52:57 +0100

    added types for RISC-V instructions

--- hardware/Lane/control_unit_lane.vhd
+++ hardware/Lane/control_unit_lane.vhd
@@ -3,13 +3,16 @@ use ieee.std_logic_1164.all;
 use work.common_pkg.all;
 
 ENTITY control_unit_lane IS
-    GENERIC(OP_LENGTH: INTEGER := 4);
+    GENERIC(
+        NR_OF_ADDR_BITS     : INTEGER := 5;    
+        OP_LENGTH           : INTEGER := 32
+    );
     PORT(
         CLK: IN STD_LOGIC;
         resetn: IN STD_LOGIC;
 
         OP                      : IN STD_LOGIC_VECTOR(OP_LENGTH-1 DOWNTO 0);
-        --REG_A,REG_B,REG_C       : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        REG_A,REG_B,REG_C       : OUT STD_LOGIC_VECTOR(NR_OF_ADDR_BITS - 1 DOWNTO 0);
         REG_IDX                 : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
         REGR,REGW               : OUT STD_LOGIC;
         DONE                    : OUT STD_LOGIC
@@ -18,7 +21,7 @@ end control_unit_lane;
 
 architecture v1 of control_unit_lane is
 
-component FSM IS
+component lane_fsm IS
     port (
         advance             :   in std_logic;
         clk                 :   in std_logic;
@@ -31,22 +34,57 @@ end component;
 SIGNAL advance : STD_LOGIC;
 SIGNAL state   : lane_state_type;
 
+-- Internal signal representations
+
+signal op_v_signal  : OP_V;
+signal ld_st_signal : LOAD_STORE_FP;
+
+signal op_type      : OP_CODE;
+
 begin
     
-    FSM1 : FSM port map (advance => advance, clk => clk, resetn => resetn, state => state);
+    op_v_signal <= (
+        funct6  => op(31 downto 26),
+        vm      => op(25),
+        field3  => op(24 downto 20),
+        field2  => op(19 downto 15),
+        op_code => op(14 downto 12),
+        field1  => op(11 downto 7)
+    );
+    
+    ld_st_signal <= (
+        nf      => op(31 downto 29),
+        mew     => op(28),
+        mop     => op(27 downto 26),
+        vm      => op(25),
+        field3  => op(24 downto 20),
+        field2  => op(19 downto 15),
+        width   => op(14 downto 12),
+        field1  => op(11 downto 7)
+    );
 
-    WITH state SELECT REG_IDX <=
+    FSM1 : lane_fsm port map (advance => advance, clk => clk, resetn => resetn, state => state);
+
+    with OP(6 downto 0) select op_type <=
+        OP_VEC      when "1010111",
+        LD_FP       when "0000111",
+        ST_FP       when others; -- "0100111";
+        
+
+    with state select REG_IDX <=
         "00" when EX1,
         "01" when EX2,
         "10" when EX3,
-        "11" when OTHERS;  
+        "11" when others;  
 
-    WITH state SELECT REGR <=
-        '0' WHEN INSTR,
-        '1' WHEN OTHERS;
+    with state select REGR <=
+        '0' when INSTR,
+        '1' when others;
 
-    WITH state SELECT REGW <=
-        '0' WHEN INSTR | EX1,
-        '1' WHEN OTHERS;
+    with state select REGW <=
+        '0' when INSTR | EX1,
+        '1' when others;
+
+    
 
 end v1;
--- hardware/common_pkg.vhd
+++ hardware/common_pkg.vhd
@@ -1,7 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 package Common_pkg is    -- untested...
 
     type lane_state_type is (INSTR, EX1, EX2, EX3, EX4, EX5);
-    type ctrl is (do, notDO);
+
+    type OP_CODE is (LD_FP, ST_FP, OP_VEC);
+
+    type LOAD_STORE_FP is record
+        nf          : std_logic_vector(2 downto 0);
+        mew         : std_logic;
+        mop         : std_logic_vector(1 downto 0);
+        vm          : std_logic;
+        field3      : std_logic_vector(4 downto 0);
+        field2      : std_logic_vector(4 downto 0);
+        width      : std_logic_vector(2 downto 0);
+        field1      : std_logic_vector(4 downto 0);
+    end record LOAD_STORE_FP;
+    
+    type OP_V is record
+        funct6      : std_logic_vector(5 downto 0);
+        vm          : std_logic;
+        field3      : std_logic_vector(4 downto 0);
+        field2      : std_logic_vector(4 downto 0);
+        op_code     : std_logic_vector(2 downto 0);
+        field1      : std_logic_vector(4 downto 0);
+    end record OP_V;
 
    -- (optional) useful tools
    --function to_slv (e : my_enum_type) return std_logic_vector;
