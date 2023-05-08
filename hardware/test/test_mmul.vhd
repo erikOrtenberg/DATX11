832d34c00223fe4e7a9625fcba4055ceca946222
Fredrik Jansson <frejanss@student.chalmers.se>
2023-05-08 12:21:56 +0200

    Minor bug fixes

--- hardware/test/test_mmul.vhd
+++ hardware/test/test_mmul.vhd
@@ -24,6 +24,7 @@ architecture V1 of mmul_tb IS
     signal op_code : std_logic_vector(31 DOWNTO 0);
     signal x_reg   : std_logic_vector(31 DOWNTO 0);
     signal done    : std_logic;
+    file opB_file: TEXT OPEN READ_MODE IS "/home/fredrik/src/DATX11/mx_example.txt"
 
 begin:
 
@@ -39,7 +40,11 @@ begin:
       clk <= not clk after 10ns;
       TEST : PROCESS
 
-      resetn <= '0';
+      VARIABLE opB_row : LINE;
+      VARIABLE opb_element : std_logic_vector(7 DOWNTO 0);
+      VARIABLE space       : CHARACTER;
+
+      resetn <= '0';!
       wait for 10ns;
       resetn <= '1';
 
@@ -47,22 +52,24 @@ begin:
           op_code <= TODO; -- Initialize register v0 to 0
           wait on done;
           wait on done;
+          readline(opB_file,opB_row);
       for j in 0 TO 31 LOOP:
       -- Load one row of operand b to v1 using VL_UNIT_STRIDE
-          op_code <= TODO;
+          x_reg   <= "00000000000000000000000000000000";
+          x_reg(19 DOWNTO 15) <= std_logic_vector(to_unsigned(j,5));
+          op_code <= "00000000000000000000000010000111";
           wait on done;
           wait on done;
-      -- Set X register to a
-          x_reg <= TODO;
+          read(opB_row, opb_element);
+          read(opB_row,space);
+      -- Set X register to a0j
+          x_reg <= opb_element;
       -- Perform one MACC operation using the VMACC.VX operation
-          op_code <= TODO;
+          op_code <= "10110100000100000110000001010111";
           wait on done;
           wait on done;
       end LOOP;
-
       -- Store the resulting row back to memory using the VS_UNIT_STRIDE instruction
       op_code <= TODO;
       wait on done;
       wait on done;
-
-
